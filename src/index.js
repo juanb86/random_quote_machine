9da8394d42aaa74529b3903445f3a213d530d4e2
juanb86 <juan.e.begnis@gmail.com>
2020-06-13 20:07:52 -0300

    Add real quotes

--- src/index.js
+++ src/index.js
@@ -4,23 +4,49 @@ import "./index.css";
 
 const DATA = [
   {
-    quote: "asdasdasd1",
-    author: "pepe1",
+    quote: "I need the secure packaging of Jockeys. My boys need a house!",
+    author: "Kramer",
   },
   {
-    quote: "asdasdasd2",
-    author: "pepe2",
+    quote:
+      "Boxers! How do you wear these things!! They’re baggin’ up, they’re rising in! And there’s nothing holding me in place! I’m flippin’! I’m floppin’!",
+    author: "Kramer",
   },
   {
-    quote: "asdasdasd3",
-    author: "pepe3",
+    quote:
+      "Looking at cleavage is like looking at the sun. You don't stare at it. It's too risky. Ya get a sense of it and then you look away.",
+    author: "Jerry Seinfeld",
+  },
+  {
+    quote:
+      "Breaking up is like knocking over a Coke machine. You can’t do it in one push; you got to rock it back and forth a few times, and then it goes over.",
+    author: "Jerry Seinfeld",
+  },
+  {
+    quote:
+      "I don't think I've ever been to an appointment in my life where I wanted the other guy to show up.",
+    author: "George Costanza",
+  },
+  {
+    quote: "It's not a lie if you believe it.",
+    author: "George Costanza",
+  },
+  {
+    quote:
+      "I’m not a lesbian! I hate men, but I’m not a lesbian.",
+    author: "Elaine Benes",
+  },
+  {
+    quote: "I'm not a lesbian. I hate men, but I'm not a lesbian",
+    author: "Elaine Benes",
   },
 ];
 
 function randomNum(current) {
-  let random = Math.floor(Math.random() * 3);
+  const maxQuotes = DATA.length;
+  let random = Math.floor(Math.random() * maxQuotes);
   while (random === current) {
-    random = Math.floor(Math.random() * 3);
+    random = Math.floor(Math.random() * maxQuotes);
   }
   return random;
 }
@@ -30,8 +56,8 @@ class Quote extends React.Component {
     const number = this.props.number;
     return (
       <div>
-        <div>{DATA[number].quote}</div>
-        <div>Author: {DATA[number].author}</div>
+        <div id="text">"{DATA[number].quote}"</div>
+        <div id="author">{DATA[number].author}</div>
       </div>
     );
   }
@@ -46,24 +72,25 @@ class QuoteMachine extends React.Component {
   }
 
   componentWillMount() {
-    this.setQuote(-1)
+    this.setQuote(-1);
   }
 
   setQuote(number) {
     const newquote = randomNum(number);
-    this.setState({currentQuote:newquote});
+    this.setState({ currentQuote: newquote });
   }
 
   render() {
     const currentQuote = this.state.currentQuote;
     return (
       <wrapper id="quote-box">
-        <div id="text">
+        <div>
           <Quote number={currentQuote} />
         </div>
-        <div id="author"> </div>
-        <button id="new-quote" onClick={()=>this.setQuote(currentQuote)}>New quote</button>
-        <a href="" id="tweet-quote">
+        <button id="new-quote" onClick={() => this.setQuote(currentQuote)}>
+          New quote
+        </button>
+        <a href="https://twitter.com/intent/tweet?text=Hello%20world" id="tweet-quote">
           Tweet Quote
         </a>
       </wrapper>
